47228938075f5fcc46411a498fae476643f8eb78
Krzysztof Bakun <krzysztof@Air-Krzysztof.lan>
2023-07-23 18:40:22 +0200

    firebase database integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from 'firebase/app';
+import { getFirestore,collection , getDocs } from 'firebase/firestore/lite';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [groups,setGrups] = useState<{id:string}[]>([])
+  const firebaseConfig = {
+    apiKey: "AIzaSyDOnz6kVAnsfXVq1wxlzuy-txEnolLWJuc",
+    authDomain: "fitmeup-f489e.firebaseapp.com",
+    databaseURL: "https://fitmeup-f489e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fitmeup-f489e",
+    storageBucket: "fitmeup-f489e.appspot.com",
+    messagingSenderId: "584222140316",
+    appId: "1:584222140316:web:1a07a96a5ae4316ad30a59",
+    measurementId: "G-CKMWWFVYE3"
+  };
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  const loadMuscleGroups = useCallback(async () => {
+    const groupsCollecion = collection(db, `muscle-groups`);
+    const list = await getDocs(groupsCollecion);
+    setGrups(list.docs.map(item=> ({
+      id: item.id
+    })))
+  },[])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>FitMeUp</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => loadMuscleGroups()}>
+          load
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {groups.map(group=><p key={group.id}>{group.id}</p>)}
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
